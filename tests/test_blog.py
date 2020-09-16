493c345e8948b91c3934f3ac5ffaa3873d50d5ce
avi09 <39293511+avi09@users.noreply.github.com>
2020-09-16 15:32:57 +0530

    Update test_blog.py

--- tests/test_blog.py
+++ tests/test_blog.py
@@ -4,7 +4,6 @@ from flaskr.db import get_db
 
 def test_index(client, auth):
     response = client.get('/index')
-    assert b"Log In" in response.data
     assert b"Register" in response.data
 
     auth.login()
