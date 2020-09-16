3ce2bb3881d94ead7133960bd1852f07ba1cf61d
avi09 <39293511+avi09@users.noreply.github.com>
2020-09-16 15:36:10 +0530

    Update test_blog.py

--- tests/test_blog.py
+++ tests/test_blog.py
@@ -3,16 +3,8 @@ from flaskr.db import get_db
 
 
 def test_index(client, auth):
-    response = client.get('/index')
-    assert b"Register" in response.data
-
-    auth.login()
-    response = client.get('/index')
-    assert b'Log Out' in response.data
-    assert b'test title' in response.data
-    assert b'by test on 2018-01-01' in response.data
-    assert b'test\nbody' in response.data
-    assert b'href="/1/update"' in response.data
+    response = client.post('/index')
+    assert response.headers['Location'] == 'http://localhost/auth/login'
 
 @pytest.mark.parametrize('path', (
     '/create',
