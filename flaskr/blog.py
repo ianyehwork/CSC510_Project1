6e4e739a7772c8ef2729ff5ceb406216a3cd0319
PushpendraPathak <69740771+PushpendraPathak@users.noreply.github.com>
2020-09-19 21:12:56 -0400

    Update blog.py

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -12,16 +12,16 @@ bp = Blueprint('blog', __name__)
 def home():
     return redirect(url_for('auth.login'))
 
-@bp.route('/index', methods=('GET','POST'))
-@login_required
-def index():
-    db = get_db()
-    posts = db.execute(
-        'SELECT p.id, title, body, created, author_id, username'
-        ' FROM post p JOIN user u ON p.author_id = u.id'
-        ' ORDER BY created DESC'
-    ).fetchall()
-    return render_template('blog/index.html', posts=posts)
+#@bp.route('/index', methods=('GET','POST'))
+#@login_required
+#def index():
+#    db = get_db()
+#    posts = db.execute(
+#        'SELECT p.id, title, body, created, author_id, username'
+#        ' FROM post p JOIN user u ON p.author_id = u.id'
+#        ' ORDER BY created DESC'
+#    ).fetchall()
+#    return render_template('blog/index.html', posts=posts)
 
 @bp.route('/create', methods=('GET', 'POST'))
 @login_required
